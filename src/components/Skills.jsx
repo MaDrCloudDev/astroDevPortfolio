fd162cd2eef6f055a4aaee23db55997ef26f6b00
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2022-12-29 23:30:32 -0700

    update skill order
    update skill order

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -283,21 +283,6 @@ const Skills = () => {
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
 								<img
-									src='https://raw.githubusercontent.com/MaDrCloudDev/projectsImages/master/netlify.png'
-									alt='/'
-									width='64'
-									height='64'
-								/>
-							</div>
-							<div className='flex flex-col items-center justify-center'>
-								<h3>Netlify</h3>
-							</div>
-						</div>
-					</div>
-					<div className='p-6 shadow-lg shadow-gray-700 rounded-xl hover:scale-105 ease-in duration-300 hover:text-yellow-400 hover:text-xl hover:font-bold cursor-default'>
-						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
-							<div className='m-auto'>
-								<img
 									src='https://raw.githubusercontent.com/MaDrCloudDev/projectsImages/master/vercel.png'
 									alt='/'
 									width='64'
@@ -306,6 +291,21 @@ const Skills = () => {
 							</div>
 							<div className='flex flex-col items-center justify-center'>
 								<h3>Vercel</h3>
+							</div>
+						</div>
+					</div>
+					<div className='p-6 shadow-lg shadow-gray-700 rounded-xl hover:scale-105 ease-in duration-300 hover:text-yellow-400 hover:text-xl hover:font-bold cursor-default'>
+						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
+							<div className='m-auto'>
+								<img
+									src='https://raw.githubusercontent.com/MaDrCloudDev/projectsImages/master/netlify.png'
+									alt='/'
+									width='64'
+									height='64'
+								/>
+							</div>
+							<div className='flex flex-col items-center justify-center'>
+								<h3>Netlify</h3>
 							</div>
 						</div>
 					</div>
