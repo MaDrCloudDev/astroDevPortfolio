fb80267285a77442be083cfc3d23a3cdac3f824c
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-05-11 15:34:58 -0700

    version update
    version update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [tailwind(), svelte(), react()]
 });
